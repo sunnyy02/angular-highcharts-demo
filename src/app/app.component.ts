48db11f8ffa88ea584331a0c874dabaa487fa692
LAPTOP-7QKP1V00\sunny <sunnyy@gmail.com>
2018-12-23 21:15:39 +1000

    add highchart demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit
+} from '@angular/core';
+import { chart, SolidGaugeChart } from 'highcharts';
+import * as Highcharts from 'highcharts';
+import * as solidGauge from 'highcharts/modules/solid-gauge.src';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
+
+  @ViewChild('chartTarget')
+  chartTarget: ElementRef;
+  options: any;
+  chart: Highcharts.ChartObject;
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.initOptions();
+    this.chart = chart(this.chartTarget.nativeElement, this.options as any);
+  }
+
+  initOptions() {
+    this.options = {
+      chart: {
+        type: 'solidgauge'
+      },
+      title: {
+        text: 'Solid Gauge Demo'
+      },
+      pane: {
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+          backgroundColor: 'white',
+          innerRadius: '60%',
+          outerRadius: '90%',
+          shape: 'arc'
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      // the value axis
+      yAxis: {
+        stops: [
+          [0.5, 'green'], // green
+          [0.6, 'yellow'], // yellow
+          [0.9, '#DF5353'] // red
+        ],
+        length: 5,
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+          y: -70
+        },
+        labels: {
+          y: 16
+        },
+        min: 0,
+        max: 200,
+        plotBands: [
+          { from: 0, to: 100, color: 'green' },
+          { from: 100, to: 120, color: 'yellow' },
+          { from: 120, to: 200, color: 'red' }
+        ]
+      },
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: 5,
+            borderWidth: 0,
+            useHTML: true
+          }
+        }
+      },
+      series: [
+        {
+          data: [80]
+        }
+      ]
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as more from 'highcharts/highcharts-more.src';
+import * as exporting from 'highcharts/modules/exporting.src';
+import * as solidGauge from 'highcharts/modules/solid-gauge.src';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartModule],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [more, solidGauge] }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
